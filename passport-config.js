7d834f1e22a3d5da2457addd3647d6df913a46e3
Pasang Tamang <pasanghilp@gmail.com>
2026-09-25 17:08:26 +0545

    Passport config

---
+++ passport-config.js
@@ -0,0 +1,12 @@
+const { authenticate } = require('passport');
+const LocalStrategy = require('passport-local');
+
+function initialize(passport){
+const authenticateUser = (email, password, done)=>{
+
+}
+
+    passport.use(new LocalStrategy({ usernameField: 'email'}), authenticateUser);
+    passport.serializeUser((user,done)=>{ })
+    passport.deserializeUser((id,done)=>{ })
+}
