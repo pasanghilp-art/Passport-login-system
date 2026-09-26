775737061d7ece7fd5a2cf3feef0f68a0ccb04ec
Pasang Tamang <pasanghilp@gmail.com>
2026-09-26 19:16:24 +0545

    update passport configuration to enhance user authentication with async handling

--- passport-config.js
+++ passport-config.js
@@ -1,12 +1,27 @@
 const { authenticate } = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
 
-function initialize(passport){
-const authenticateUser = (email, password, done)=>{
-
+function initialize(passport, getUserByEmail, getUserById){
+const authenticateUser = async (email, password, done)=>{
+    const user = await getUserByEmail(email);
+    if(user == null){
+        return done(null, false, { message: 'No user with that email'});
+    }
+    try {
+        if ( await bcrypt.compare(password, user.password)) {
+            return done(null, user)
+        } else {
+            return done(null, false, { message: 'password incorrect'});
+        }
+    } catch(e) {
+        return done(e)
+    }
 }
 
-    passport.use(new LocalStrategy({ usernameField: 'email'}), authenticateUser);
-    passport.serializeUser((user,done)=>{ })
-    passport.deserializeUser((id,done)=>{ })
+    passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser));
+    passport.serializeUser((user,done)=> done(null, user.id))
+    passport.deserializeUser((id,done)=> done(null, getUserById(id)))
 }
+
+module.exports = initialize;
